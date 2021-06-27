73204147aaba1b470df56e26f626713fd1dad44a
Cristina Reyes Saravia <crie.reyes@duouc.cl>
2021-06-27 19:57:05 -0400

    Se crea crear animal /Reyes

--- Solemne_3/core/views.py
+++ Solemne_3/core/views.py
@@ -11,3 +11,16 @@ def views_animal(request):
         'animales' : animales
     }
     return render(request, 'core/views_animal.html', datos)
+
+#formularo para crear animal
+def add_animal(request):
+    datos = {
+        'form': AnimalForm() 
+        }
+    if request.method == 'POST':
+        formulario_add = AnimalForm(request.POST)
+        if formulario_add.is_valid:
+            formulario_add.save()
+            datos['mensaje'] = "Animal agregado correctamente"
+
+    return render(request, 'core/add_animal.html', datos)
