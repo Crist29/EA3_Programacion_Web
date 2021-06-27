70f5457eafc0423ecf68debf589697bd23fa071e
abalvina <ab.alvina@duocuc.cl>
2021-06-27 19:54:07 -0400

    SE CREA CRUD VER ANIMAL / AALVINA

--- Solemne_3/core/views.py
+++ Solemne_3/core/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import request
+from django.shortcuts import render, redirect
+from .models import Animal
+from .forms import AnimalForm
 
-# Create your views here.
+#listado de Animales
+def views_animal(request):
+    animales = Animal.objects.all()
+    datos = {
+        'animales' : animales
+    }
+    return render(request, 'core/views_animal.html', datos)
