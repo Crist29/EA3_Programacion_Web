69b16b27fee78133e470dae84cfd476f8891553e
Damaris Rodrigue <dama.rodriguez@duocuc.cl>
2021-06-27 19:26:23 -0400

    se crea categoria- rodriguez

--- Solemne_3/core/models.py
+++ Solemne_3/core/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Categoria(models.Model):
+    idCategoria = models.IntegerField(primary_key=True, verbose_name="Id categoria")
+    nombreCategoria = models.CharField(max_length=50, verbose_name="Nombre categoria")
+
+    def _str_(self):
+        return self.nombreCategoria
