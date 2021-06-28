ca301323b13f101bfbdf42a3ca0f1e1e46b18fb3
PlayerCode01 <o.ramirez.carlos@gmail.com>
2021-06-27 21:09:30 -0400

    Revisado y cambiadas las urls

--- Solemne_3/Solemne_3/urls.py
+++ Solemne_3/Solemne_3/urls.py
@@ -13,9 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from core.views import views_animal, add_animal, delete_animal, edit_animal, home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home, name="home"),
+    path('agregar_animal/', add_animal, name="add_animal" ),
+    path('editar_animal/<pk>', edit_animal, name="edit_animal" ),
+    path('eliminar_animal/<pk>', delete_animal, name="delete_animal" ),
+    path('views_animal/', views_animal, name="views_animal")
 ]
