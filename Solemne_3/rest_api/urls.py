57d197037811c3b4e36327eae148ba593e8dcc7d
Cristina Reyes Saravia <crie.reyes@duouc.cl>
2021-07-07 19:41:19 -0400

    Se agrega login a la url de la api / Reyes

--- Solemne_3/rest_api/urls.py
+++ Solemne_3/rest_api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from rest_api.views import animales,animal
+from .viewsLogin import login
 
 urlpatterns = [
     path('animal/', animales, name='animales'),
     path('animal/<pk>', animal, name="animal"),
+    path('login/', login, name='login'),
 ]
