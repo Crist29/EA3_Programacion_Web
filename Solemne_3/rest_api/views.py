e4f10b419913ba7f4af6c4289e03b5c08890baf3
abalvina <ab.alvina@duocuc.cl>
2021-07-06 23:43:08 -0400

    SE AGREGA LISTADO DE AGREGA ANIMAL  AL VIEW

--- Solemne_3/rest_api/views.py
+++ Solemne_3/rest_api/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from .serializers import AnimalSerializer
+from core.models import Animal
+from rest_framework.response import Response
+from rest_framework.parsers import JSONParser
+from rest_framework import status
+from rest_framework.decorators import api_view
 
-# Create your views here.
+@api_view(['GET', 'POST'])
+def animales(request):
+    """
+    Lista todos los animales
+    """
+    if request.method == 'GET':
+        animales_lista = Animal.objects.all()
+        serializer = AnimalSerializer(animales_lista, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        """
+        Agrega un animal
+        """
+        data = JSONParser().parse(request)
+        serializer = AnimalSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
