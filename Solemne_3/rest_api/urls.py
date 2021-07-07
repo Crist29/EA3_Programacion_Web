73d6de383d16f1ef70d7f1fb97a05a56f2553d88
Cristina Reyes Saravia <crie.reyes@duouc.cl>
2021-07-07 19:18:02 -0400

    Se agrega funcion animmal a la url/Reyes

--- Solemne_3/rest_api/urls.py
+++ Solemne_3/rest_api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from rest_api.views import animales
+from rest_api.views import animales,animal
 
 urlpatterns = [
     path('animal/', animales, name='animales'),
+    path('animal/<pk>', animal, name="animal"),
 ]
